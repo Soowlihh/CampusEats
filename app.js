6c9bfafcbb730def6e99b7710fdc81659cebe8af
Soowlih Compaore <scompaore1@gmail.com>
2026-01-10 03:43:21 -0500

    Updated project files

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utilities/catchAsync');
+const expressError = require('./utilities/expressError');
 const methodOverride = require('method-override');
 const Campuseats = require('./models/campuseats');
 
 mongoose.connect('mongodb://localhost:27017/campus', {
-    useNewUrlParser: true,
+   // useNewUrlParser: true,
    // useCreateIndex: true,
-    useUnifiedTopology: true
+   // useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -17,45 +20,69 @@ db.once("open", () => {
 });
 const app = express();
  
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-app.use(express.urlencoded({extended : true}))
+
+app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
 
- 
-
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+  });
 app.get('/', (req, res) => {
-    res.render('home')
-})
+    res.redirect('/campuseats');
+  });
+//app.get('/', (req, res) => {
+ //   res.render('home')
+//})
 app.get('/campuseats', async (req, res) => {
     const campuseats = await Campuseats.find({});
     console.log(campuseats);
     res.render('campuseats/index', {campuseats})
 })
 app.get('/campuseats/new', (req, res) => {
+
     // const campuseats = await Campuseats.findById(req.params.id)
      res.render('campuseats/new');
  })
- app.post('/campuseats', async (req, res) => {
+ app.post('/campuseats', catchAsync(async (req, res, next) => {
+    if(!req.body.campuseat) throw new expressError('Invalid Data' , 400)
     const campuseat = new Campuseats(req.body.campuseats);
     await campuseat.save();
     res.redirect(`/campuseats/${campuseat._id}`)
- })
+ }))
  
 app.get('/campuseats/:id', async(req, res) => {
     const campuseats = await Campuseats.findById(req.params.id)
     res.render('campuseats/show', {campuseats});
 })
-app.get('/campuseats/:id/edit', async(req, res) => {
+app.get('/campuseats/:id/edit', catchAsync( async(req, res) => {
     const campuseats = await Campuseats.findById(req.params.id)
     res.render('campuseats/edit', {campuseats});
-})
-app.put('/campuseats/:id', async (req, res) => {
+}))
+app.put('/campuseats/:id', catchAsync( async (req, res) => {
     //res.send("Worked!!!")
     const{id} = req.params;
-    const campuseats = await Campuseats.findByIdAndUpdate(id,{...req.body.campuseats});
-    res.redirect(`/campuseats/${campuseats._id}`)
+    const campuseat = await Campuseats.findByIdAndUpdate(id,{...req.body.campuseats});
+    res.redirect(`/campuseats/${campuseat._id}`)
+}))
+app.delete('/campuseats/:id', catchAsync( async (req, res) =>{
+    const {id} = req.params;
+    await Campuseats.findByIdAndDelete(id);
+    res.redirect('/campuseats');
+}))
+
+app.all(/(.*)/, (req,  res, next) => {
+    next(new expressError('Page Not Found' , 404))
 })
-app.listen(3000, () => {
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('ERROR', {err})
+})
+
+app.listen(4000, () => {
     console.log('Serving on port 3000')
 }) 
--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campuseats.deleteMany({});
    for (let i = 0; i < 10; i++){
     const random10 = Math.floor(Math.random() * 10);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campuseats({
         location: `${spots[random10].name}, ${spots[random10].address}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image: `https://picsum.photos/400?random=${Math.random()}`,
+        description: 'Step into Halal Spot, where every dish is prepared with care, using only 100% Halal-certified ingredients. From sizzling grills to savory specialties, our menu brings authentic flavors from around the world right to your plate. Whether you’re grabbing a quick bite, enjoying a hearty meal with friends, or craving comfort food, Halal Spot promises fresh, flavorful, and wholesome meals that satisfy both your taste buds and your values.',
+        price
     })
     await camp.save();
    }
